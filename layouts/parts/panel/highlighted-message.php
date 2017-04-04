722baeaedd8709b5ec272d55bb214ce55e56a1f5
Gustavo Lima da Silva <gustavo.silva.vs@gmail.com>
2017-04-04 14:18:43 -0300

    Fix panel index message for Subsite

---
+++ layouts/parts/panel/highlighted-message.php
@@ -0,0 +1,9 @@
+<p class="highlighted-message">
+    <?php \MapasCulturais\i::_e("Olá");?>, <a href="<?php echo $app->user->profile->singleUrl ?>"><?php echo $app->user->profile->name ?></a>, bem-vindo ao Mapa da Cultura, o novo cadastro do SNIIC!
+    <br/>
+    <br/>
+    O Nº SNIIC do seu agente padrão "<?php echo $app->user->profile->name ?>" é <?php echo $app->user->profile->num_sniic ?>.
+    <br/>
+    <br/>
+    <small>Clique nos itens abaixo para visualizar, consultar o número SNIIC e editar as informações cadastradas.</small>
+</p>
